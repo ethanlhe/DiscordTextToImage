c88c45efd842f375b0ba81e7ae0f8892d881fc87
ethanlhe <ethansp123@gmail.com>
2023-03-28 17:00:00 +0000

    warnings

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import asyncio
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # no warnings
+
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), application_id="1013232498249043969")
 
 @bot.event
