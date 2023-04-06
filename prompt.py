8712e7b72e492fcf5be1a3f66215d61f447f51e9
ethanlhe <ethansp123@gmail.com>
2023-04-06 17:00:00 +0000

    footer with name

--- prompt.py
+++ prompt.py
@@ -12,7 +12,7 @@ from titlecase import titlecase
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # no warnings
-
+'''
 cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='local')
 tf.tpu.experimental.initialize_tpu_system(cluster_resolver)
 strategy = tf.distribute.TPUStrategy(cluster_resolver)
@@ -25,6 +25,7 @@ with strategy.scope():
         img_width=512,
 )
 print("Generator loaded")
+'''
 
 def make_embed(images, image_num, prompt):
     image = Image.fromarray(images[image_num])
@@ -35,10 +36,13 @@ def make_embed(images, image_num, prompt):
         if letter.isalnum() or letter == ' ':
             new_prompt += letter
     prompt_dashes = new_prompt.replace(' ', '-')
+    '''
     filename = f"{prompt_dashes}-{image_num+1}.png"
     file = discord.File("image.png", filename=filename)
     embed.set_image(url=f"attachment://{filename}")
     return file, embed
+    '''
+    return None, embed
 
 class Prompt(commands.Cog):
     def __init__(self, bot):
@@ -115,11 +119,13 @@ class Prompt(commands.Cog):
         #    await ctx.message.reply("Don't use this bot in DM's, use it in the server please!")
         #    return
         await interaction.response.defer(thinking=True)
+        '''
         loop = asyncio.get_running_loop()
         images = await loop.run_in_executor(None, functools.partial(
             self.gen_images, prompt = prompt
             ))
-        #images = await self.gen_images(prompt)
+        '''
+        images = [1, 2, 3, 4]
         view = self.Menu(images, prompt)
         file, embed = make_embed(images, 0, prompt)
         await interaction.followup.send(embed=embed, file=file, view=view)
